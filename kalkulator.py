1755afa989aedd45c2d095c0f4a3442601167f5a
mutapakkir <151417931+mutapakkir@users.noreply.github.com>
2024-03-20 03:19:32 -0700

    Add files via upload

---
+++ kalkulator.py
@@ -0,0 +1,19 @@
+# latihan konversi suhu
+#program konversi celcius
+nama = 'MUHAMMAD IBDAUL MUTAFAKKIR'
+matkul = 'Dasar Pemograman'
+print(f'Hallo selamat Datang {nama},selamat belajar {matkul}')
+
+print("\nPROGRAM KONVERSI TEMPERATUR\n")
+
+celcius = float(input('Masukkan suhu dalam celcius :'))
+print("suhu adalah ",celcius,"C")
+
+reamur = (4/5)* celcius
+print("suhu dalam reamur adalah ",reamur,"R")
+
+fahrenheit = (9/5)*celcius +32
+print("suhu dalam fahrenheit adalah ",fahrenheit,"F")
+
+kelvin = celcius + 273
+print("suhu dalam Kelvin adalah ",kelvin,"F")
